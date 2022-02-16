02ae36424e5aa18caedb31902ace20376b4b8187
Jeff Lin <jeffylin@xilinx.com>
2022-02-16 00:25:13 -0800

    Unlock metadata BO in zocl_sk on cleanup

--- src/runtime_src/core/edge/drm/zocl/zocl_sk.c
+++ src/runtime_src/core/edge/drm/zocl/zocl_sk.c
@@ -205,6 +205,9 @@ zocl_fini_soft_kernel(struct drm_zocl_dev *zdev)
 	sk = zdev->soft_kernel;
 	mutex_lock(&sk->sk_lock);
 
+	if(!IS_ERR(&sk->sk_meta_bo))
+		ZOCL_DRM_GEM_OBJECT_PUT_UNLOCKED(&sk->sk_meta_bo->gem_base);		
+	
 	for (i = 0; i < sk->sk_nimg; i++) {
 		if (IS_ERR(&sk->sk_img[i].si_bo))
 			continue;
